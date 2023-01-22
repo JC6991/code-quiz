5a8e08de733e9a51e51a713d08f66c67a3f45799
Joshua Crehan <joshua.crehan@yahoo.com>
2023-01-22 11:05:08 +0000

    display question and answers options when user clciks start

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,15 @@
 //Selecting elements by id
-let startButton = document.querySelector("#start")
+const startButton = document.querySelector("#start");
 let timeEl = document.querySelector("#time");
+const startScreen = document.querySelector("#start-screen");
+let questionTitle = document.querySelector("#question-title");
+let choices = document.querySelector("#choices");
+const questions = document.querySelector("#questions")
 
 function startGame() {
     startButton.disabled = true;
     startTimer();
+    startQuiz();
 }
 
 let secondsLeft = 10;
@@ -19,11 +24,58 @@ function startTimer() {
       if(secondsLeft === 0) {
         // Stops execution of action at set interval
         clearInterval(timerInterval);
-        // Calls function to create and append image        
+            
       }
   
     }, 1000);
   }
 
-// Attach event listener to start button to call startGame function on click
+  function startQuiz() {
+    startScreen.remove();
+    document.getElementById('questions').style.display='block'; 
+           
+    questionTitle.textContent = questionOne();
+    // document.body.append(questionTitle);
+
+    // Create ordered list element
+    const olEl = document.createElement("ol")
+    
+    // Create list items
+    let liEl1 = document.createElement("li");
+    let liEl2 = document.createElement("li");
+    let liEl3 = document.createElement("li");
+    let liEl4 = document.createElement("li");
+
+    // add content for list items
+    liEl1.textContent = quizQuestions[0].answers.one;
+    liEl2.textContent = quizQuestions[0].answers.two;
+    liEl3.textContent = quizQuestions[0].answers.three;
+    liEl4.textContent = quizQuestions[0].answers.four;
+
+    // append ol element choices id
+    choices.appendChild(olEl);
+
+    // append list items to ordered list element
+    olEl.appendChild(liEl1);
+    olEl.appendChild(liEl2);
+    olEl.appendChild(liEl3);
+    olEl.appendChild(liEl4);
+
+    // Add styling to list element
+// olEl.setAttribute("style", "background:#333333; padding:20px;");
+// // Add styling to list items
+// liEl1.setAttribute("style", " color:white; background: #666666; padding: 5px; margin-left: 35px;");
+// liEl2.setAttribute("style", " color:white; background: #777777; padding: 5px; margin-left: 35px;");
+// liEl3.setAttribute("style", " color:white; background: #888888; padding: 5px; margin-left: 35px;");
+// liEl4.setAttribute("style", " color:white; background: #999999; padding: 5px; margin-left: 35px;");
+
+
+    // let ansLength = Object.keys(quizQuestions[0].answers)    
+    // for(i = 0; i < ansLength; i++) {
+    //   let liEl1 = document.createElement("li")
+    // }
+    
+  }
+
+// Event listener to call startGame function when startButton is clicked
 startButton.addEventListener("click", startGame);
